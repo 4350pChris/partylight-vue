61d3e4fefba8a271f3636cba64bfd02c533034c3
Chris-Robin Ennen <chris@ncteam.de>
2018-12-24 16:52:24 +0100

    changed settings list to use v-container layout

--- src/components/SettingsList.vue
+++ src/components/SettingsList.vue
@@ -1,17 +1,40 @@
 <template>
-  <div>
-    <div v-for="(value, key) in settings" :key="key">
-      <h2>{{key}}: {{value}}</h2>
-    </div>
-  </div>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card 
+          v-for="(value, key) in settings"
+          :key="key"
+        >
+          <v-card-title>{{ key | capitalize }}</v-card-title>
+          <v-card-text>
+            <v-slider
+            :value="value"
+            @input="updateSetting(key, $event)"
+            thumb-label="always"
+          ></v-slider>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { Settings } from '@/api/settings';
+import { MUTATIONS } from '@/store/settings/mutations';
 
 @Component
 export default class SettingsList extends Vue {
-  private settings: Settings = this.$store.state.settings;
+  get settings() {
+    return this.$store.state.settings;
+  }
+
+  public updateSetting(key: keyof Settings, value: any) {
+    const cpy = this.settings;
+    cpy[key] = value;
+    this.$store.commit(MUTATIONS.SET_SETTINGS, cpy);
+  }
 }
 </script>
