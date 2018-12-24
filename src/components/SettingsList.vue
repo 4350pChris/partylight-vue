1e76f81d57b134f0c6252c8e40df3f1907aa5dfb
Chris-Robin Ennen <chris@ncteam.de>
2018-12-24 13:52:34 +0100

    added settingslist skeleton

---
+++ src/components/SettingsList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-for="(value, key) in settings" :key="key">
+      <h2>{{key}}: {{value}}</h2>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Settings } from '@/api/settings';
+
+@Component
+export default class SettingsList extends Vue {
+  private settings: Settings = this.$store.state.settings;
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,18 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/splash.png">
     <script-editor/>
+    <settings-list/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import ScriptEditor from '@/components/ScriptEditor.vue';
+import SettingsList from '@/components/SettingsList.vue';
 
 @Component({
   components: {
+    SettingsList,
     ScriptEditor,
   },
 })
