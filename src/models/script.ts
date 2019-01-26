76873db791fd5b9c19981d75696b66b612c996e0
Chris-Robin Ennen <chris@ncteam.de>
2019-01-26 15:45:40 +0100

    added models

---
+++ src/models/script.ts
@@ -0,0 +1,5 @@
+export default interface Script {
+    id: string;
+    name: string;
+    code: string;
+}
---
+++ src/models/settings.ts
@@ -0,0 +1,5 @@
+export default interface Settings {
+  brightness: number;
+  color: number;
+  delay: number;
+}
