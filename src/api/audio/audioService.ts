66077135efd320a8ade2d2b4ea69924d9d60a069
Chris-Robin Ennen <chris@ncteam.de>
2019-02-03 22:43:43 +0100

    added audio service

---
+++ src/api/audio/audioService.ts
@@ -0,0 +1,13 @@
+import { BaseSocketService } from '../baseSocketService';
+import AudioParameters from '@/models/audioParameters';
+
+
+export default class AudioService extends BaseSocketService {
+  public getAudioParameters = () => this.invoke<AudioParameters>('GetAudioParameters');
+
+  public setAudioParameters = (params: AudioParameters) => this.invoke<void>('SetAudioParameters', params);
+
+  private getAudioBuffer = () => this.invoke<number[]>('GetAudioBuffer');
+
+  private setAudioBuffer = (buffer: number[]) => this.invoke<void>('SetAudioBuffer', buffer);
+}
