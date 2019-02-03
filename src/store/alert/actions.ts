aba6090001b927293acb5673a0d18e3bc0899f54
Chris-Robin Ennen <chris@ncteam.de>
2019-02-03 16:53:10 +0100

    triggering multiple alerts caused later ones to not show up. This is due to earlier calls to setTimeout setting the alert's visiblity to false even though it was just set to true by a more recent invocation. The solution is debouncing the hidealert function to wait at least 2 secs before triggering on subsequent calls

--- src/store/alert/actions.ts
+++ src/store/alert/actions.ts
@@ -1,18 +1,23 @@
 import { ActionTree } from 'vuex';
 import { State } from '.';
 import { Mutations } from '.';
+import { debounce } from 'lodash';
 
 export enum Actions {
-  ShowAlert = 'showAlert'
+  ShowAlert = 'showAlert',
+  HideAlert = 'hideAlert'
 }
 
 const actions: ActionTree<State, any> = {
-  [Actions.ShowAlert]({ commit }, payload: { type: string, duration?: number, message: string }) {
+  [Actions.ShowAlert]({ commit, dispatch }, payload: { type: string, duration?: number, message: string }) {
     commit(Mutations.SetMessage, payload.message);
     commit(Mutations.SetType, payload.type);
     commit(Mutations.SetVisibility, true);
-    setTimeout(() => commit(Mutations.SetVisibility, false), payload.duration || 4000);
-  }
+    setTimeout(() => dispatch(Actions.HideAlert), payload.duration);
+  },
+
+  [Actions.HideAlert]: debounce(({ commit }) => commit(Mutations.SetVisibility, false), 2000),
+
 };
 
 export default actions;
