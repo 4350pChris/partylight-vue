ffb6b14b4c6b0025a67fbebc910a54d12f14097f
Chris-Robin Ennen <chris@ncteam.de>
2019-02-06 12:21:47 +0100

    forgot to remove buffer actions

--- src/store/audio/actions.ts
+++ src/store/audio/actions.ts
@@ -6,26 +6,15 @@ import AudioService from '../../api/audio/audioService';
 const audioService = ServiceFactory.get('audio') as AudioService;
 
 export enum Actions {
-  FetchBuffer = 'fetchBuffer',
   FetchParameters = 'fetchParameters',
-  SaveBuffer = 'saveBuffer',
   SaveParameters = 'saveParameters'
 }
 
 const actions: ActionTree<State, any> = {
-  async [Actions.FetchBuffer]({ commit }) {
-    const buffer = await audioService.getAudioBuffer();
-    commit(Mutations.SetBuffer, buffer);
-  },
 
   async [Actions.FetchParameters]({ commit }) {
     const parameters = await audioService.getAudioParameters();
     commit(Mutations.SetParameters, parameters);
-  },
-
-  [Actions.SaveBuffer]({ commit }, payload: number[]) {
-    commit(Mutations.SetBuffer, payload);
-    return audioService.setAudioBuffer(payload);
   },
 
   [Actions.SaveParameters]({ commit }, payload) {
