ece7d91331cd6fce3b45742a1f31cc3b0e34207e
Chris-Robin Ennen <chris@ncteam.de>
2019-02-10 17:02:15 +0100

    made roboto font and material design icons available offline

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './filters';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.config.productionTip = false;
 
