05cf28d272f6fc9aab98fb027dd21b880e2dcebf
Chris-Robin Ennen <chris@ncteam.de>
2019-02-10 17:49:28 +0100

    unnecessary import

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/src/stylus/app.styl';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.use(Vuetify, {
   iconfont: 'md',
