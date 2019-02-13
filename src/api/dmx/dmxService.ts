dd786562c1c6341dcbddaa241806c0a034d95e8d
Chris-Robin Ennen <chris@ncteam.de>
2019-02-13 08:31:58 +0100

    added hub methods

--- src/api/dmx/dmxService.ts
+++ src/api/dmx/dmxService.ts
@@ -5,6 +5,14 @@ export default class DMXService extends BaseSocketService {
 
   public getPacketsPerSecond = () => this.invoke<number>('GetPacketsPerSecond');
 
+  public getMaximumFrequency = (length: number) => this.invoke<number>('CalculateMaximumFrequency', length);
+
+  public getUsedFrequency = () => this.invoke<number>('CalculateUsedFrequency');
+
+  public getLengthOfUniverse = () => this.invoke<number>('GetLengthOfUniverse');
+
+  public setLengthOfUniverse = (length: number) => this.invoke<boolean>('SetLengthOfUniverse', length);
+
   public setSamplingRate = (rate: number) => this.invoke<boolean>('SetSamplingRate', rate);
 
   public onChange = (cb: (prop: string, value: number) => void) => this.on('PropertyChanged', cb);
