e3c7bf2da1b201bff82b200120fded0778f8c807
Chris-Robin Ennen <chris@ncteam.de>
2019-04-02 08:12:46 +0200

    editor supports dark theme editor resizes to take up full width and height of surrounding div

--- src/components/editor/ScriptEditor.vue
+++ src/components/editor/ScriptEditor.vue
@@ -1,20 +1,31 @@
 <template>
-  <!-- <monaco-editor class="editor" v-model="code" language="csharp" ref="editor"></monaco-editor> -->
   <div id="editor"></div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Watch, Vue } from 'vue-property-decorator';
+import { Component, Inject, Prop, Watch, Vue } from 'vue-property-decorator';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import { IPosition, editor, languages } from 'monaco-editor';
 import services from '@/api';
 
 @Component
 export default class ScriptEditor extends Vue {
+  @Inject()
+  private theme!: { isDark: boolean };
+
+  private get editorTheme() {
+    return this.theme.isDark ? 'vs-dark' : 'vs';
+  }
+
   @Prop()
   private value!: string;
 
   private code: string = this.value || '';
+
+  @Watch('theme', { deep: true })
+  private onThemeChanged() {
+    monaco.editor.setTheme(this.editorTheme);
+  }
 
   @Watch('value')
   private onValueChanged() {
@@ -32,7 +43,10 @@ export default class ScriptEditor extends Vue {
     });
     monaco.editor.create(document.getElementById('editor') as HTMLElement, {
         value: this.code,
-        language: 'csharp'
+        language: 'csharp',
+        theme: this.editorTheme,
+        scrollBeyondLastLine: false,
+        automaticLayout: true
     });
   }
 }
