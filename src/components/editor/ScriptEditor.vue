f7b3aa04f2dcb40bf34da2807176dbcbc420b411
Chris-Robin Ennen <chris@ncteam.de>
2019-04-02 09:58:21 +0200

    added proper autocompletion and icons

--- src/components/editor/ScriptEditor.vue
+++ src/components/editor/ScriptEditor.vue
@@ -7,6 +7,7 @@ import { Component, Inject, Prop, Watch, Vue } from 'vue-property-decorator';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import { IPosition, editor, languages } from 'monaco-editor';
 import services from '@/api';
+import Completion from '@/models/completion';
 
 @Component
 export default class ScriptEditor extends Vue {
@@ -37,7 +38,33 @@ export default class ScriptEditor extends Vue {
       provideCompletionItems: async (model, position) => {
         const source = model.getValue();
         const pos = model.getOffsetAt(position);
-        const suggestions  = await services.scripts.getCompletionItems(model.getValue(), pos);
+        const completions: Completion[]  = await services.scripts.getCompletionItems(model.getValue(), pos);
+        const suggestions: languages.CompletionItem[] = completions.map(i => {
+          const kind: languages.CompletionItemKind =
+            languages.CompletionItemKind[i.kind as keyof typeof languages.CompletionItemKind];
+          const wordAtPos: editor.IWordAtPosition = model.getWordAtPosition(position) as editor.IWordAtPosition;
+          let startColumn: number;
+          let endColumn: number;
+          if (wordAtPos === null) {
+            startColumn = position.column;
+            endColumn = position.column;
+          } else {
+            startColumn = wordAtPos.startColumn;
+            endColumn = wordAtPos.endColumn;
+          }
+          const range: monaco.IRange = {
+            startLineNumber: position.lineNumber,
+            endLineNumber: position.lineNumber,
+            startColumn,
+            endColumn
+          };
+          return {
+            label: i.label,
+            insertText: i.label,
+            kind,
+            range
+          };
+        });
         return { suggestions } as languages.CompletionList;
       }
     });
