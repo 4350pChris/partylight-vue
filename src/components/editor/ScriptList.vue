d7a98189916a20e5eb641ec3e15305396bd18bf1
Chris-Robin Ennen <chris@ncteam.de>
2019-04-04 10:57:12 +0200

    formatting

--- src/components/editor/ScriptList.vue
+++ src/components/editor/ScriptList.vue
@@ -1,25 +1,25 @@
 <template>
   <v-card>
     <v-slide-y-transition class="py-0" group tag="v-list">
-        <v-list-tile @click="$emit('new')" ripple key="-1">
-          <v-list-tile-content>
-            <v-list-tile-title>New Script</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile
-          v-for="script in scripts"
-          :key="script.id"
-          @click="$emit('select', script)"
-          ripple
-          :color="isActiveScript(script) ? 'primary' : ''"
-        >
-          <v-list-tile-content>
-            <v-list-tile-title>{{ script.name }}</v-list-tile-title>
-          </v-list-tile-content>
-          <v-scroll-x-transition>
-            <v-icon v-if="isActiveScript(script)" color="success">check_circle</v-icon>
-          </v-scroll-x-transition>
-        </v-list-tile>
+      <v-list-tile @click="$emit('new')" ripple key="-1">
+        <v-list-tile-content>
+          <v-list-tile-title>New Script</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile
+        v-for="script in scripts"
+        :key="script.id"
+        @click="$emit('select', script)"
+        ripple
+        :color="isActiveScript(script) ? 'primary' : ''"
+      >
+        <v-list-tile-content>
+          <v-list-tile-title>{{ script.name }}</v-list-tile-title>
+        </v-list-tile-content>
+        <v-scroll-x-transition>
+          <v-icon v-if="isActiveScript(script)" color="success">check_circle</v-icon>
+        </v-scroll-x-transition>
+      </v-list-tile>
     </v-slide-y-transition>
   </v-card>
 </template>
