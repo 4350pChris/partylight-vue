89117759606b89abea2cd228b74d7f742d475ba4
Chris-Robin Ennen <chris@ncteam.de>
2019-04-05 20:04:55 +0200

    loading button that gets passed a click handler and displays the loading icon while the function executes

---
+++ src/components/shared/LoadingButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-btn @click="click()" :loading="loading" v-bind="buttonOptions">
+    <slot></slot>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class LoadingButton extends Vue {
+  private loading: boolean = false;
+
+  @Prop()
+  private clickHandler!: CallableFunction;
+
+  @Prop()
+  private buttonOptions!: { [prop: string]: any };
+
+  private async click() {
+    this.loading = true;
+    let result: any;
+    try {
+      result = await this.clickHandler();
+      return result;
+    } catch (e) {
+      throw e;
+    } finally {
+      this.loading = false;
+    }
+  }
+}
+</script>
+
