53dba808e88b66673eb25487d6500eff6da77dff
Chris-Robin Ennen <chris@ncteam.de>
2019-07-10 12:13:28 +0200

    use vue-monaco as editor

--- src/components/editor/ScriptEditor.vue
+++ src/components/editor/ScriptEditor.vue
@@ -1,37 +1,37 @@
 <template>
-  <div id="editor" v-resize="resizeEditor"></div>
+  <MonacoEditor
+    id="editor"
+    v-model="syncedCode"
+    language="csharp"
+    :theme="editorTheme"
+    :options="options"
+  />
 </template>
 
 <script lang="ts">
-import { Component, Prop, Watch, Mixins } from 'vue-property-decorator';
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import { IPosition, editor, languages } from 'monaco-editor';
+import { Component, PropSync, Watch, Mixins } from 'vue-property-decorator';
+import MonacoEditor from 'vue-monaco';
+import { IPosition, IRange, editor, languages } from 'monaco-editor';
 import services from '@/api';
 import Completion from '@/models/completion';
 import ThemeMixin from '@/mixins/theme';
 
-@Component
+@Component({ components: { MonacoEditor }})
 export default class ScriptEditor extends Mixins(ThemeMixin) {
-  editor: editor.IStandaloneCodeEditor | null = null;
+  @PropSync('code', { type: String })
+  syncedCode!: string;
 
-  @Prop()
-  value!: string;
+  options: editor.IEditorConstructionOptions = {
+    scrollBeyondLastLine: false,
+    minimap: { enabled: false }
+  };
 
-  @Watch('theme', { deep: true })
-  onDarkChanged(val: boolean) {
-    const theme = this.theme.isDark ? 'vs-dark' : 'vs';
-    monaco.editor.setTheme(theme);
-  }
-
-  @Watch('value')
-  onValueChanged(val: string, oldVal: string) {
-    if (this.editor !== null && val !== this.editor.getValue()) {
-      this.editor.setValue(val);
-    }
+  get editorTheme() {
+    return this.theme.isDark ? 'vs-dark' : 'vs';
   }
 
   registerCompletion() {
-    monaco.languages.registerCompletionItemProvider('csharp', {
+    languages.registerCompletionItemProvider('csharp', {
       provideCompletionItems: async (model, position) => {
         const source = model.getValue();
         const pos = model.getOffsetAt(position);
@@ -49,7 +49,7 @@ export default class ScriptEditor extends Mixins(ThemeMixin) {
             startColumn = wordAtPos.startColumn;
             endColumn = wordAtPos.endColumn;
           }
-          const range: monaco.IRange = {
+          const range: IRange = {
             startLineNumber: position.lineNumber,
             endLineNumber: position.lineNumber,
             startColumn,
@@ -65,41 +65,6 @@ export default class ScriptEditor extends Mixins(ThemeMixin) {
         return { suggestions } as languages.CompletionList;
       }
     });
-  }
-
-  createEditor(): editor.IStandaloneCodeEditor {
-    const options: monaco.editor.IEditorConstructionOptions = {
-      value: this.value,
-      language: 'csharp',
-      theme: this.theme.isDark ? 'vs-dark' : 'vs',
-      scrollBeyondLastLine: false,
-      minimap: { enabled: false }
-    };
-    return monaco.editor.create(
-      document.getElementById('editor') as HTMLElement,
-      options
-    );
-  }
-
-  registerInputListener() {
-    if (this.editor !== null) {
-      const e = this.editor;
-      e.onDidChangeModelContent(() => {
-        this.$emit('input', e.getValue());
-      });
-    }
-  }
-
-  resizeEditor() {
-    if (this.editor !== null) {
-      this.editor.layout();
-    }
-  }
-
-  mounted() {
-    this.registerCompletion();
-    this.editor = this.createEditor();
-    this.registerInputListener();
   }
 }
 </script>
