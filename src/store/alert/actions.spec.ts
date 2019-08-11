7f983eb1de19b548775516646e724b1ec90d4f5d
Chris-Robin Ennen <chris@ncteam.de>
2019-08-11 15:18:44 +0200

    fix actions to use lolex

--- src/store/alert/actions.spec.ts
+++ src/store/alert/actions.spec.ts
@@ -2,37 +2,37 @@ import alertStore, { Actions, State, state } from '@/store/alert';
 import Vuex, { Store } from 'vuex';
 import { createLocalVue } from '@vue/test-utils';
 import { cloneDeep } from 'lodash';
-
-const localVue = createLocalVue();
-
-localVue.use(Vuex);
-jest.useRealTimers();
+import lolex from 'lolex';
 
 describe('Alert Store Actions', () => {
   let store: Store<State>;
+  let clock: lolex.InstalledClock;
 
   beforeEach(() => {
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
+    clock = lolex.install();
     store = new Vuex.Store(cloneDeep(alertStore));
   });
 
-  it('Show Alert', done => {
+  afterEach(() => {
+    clock.uninstall();
+  });
+
+  it('Show Alert', () => {
     const payload = { type: 'error', message: 'Test', duration: 0 };
     store.dispatch(Actions.ShowAlert, payload);
-    // alert should be visible right after the call
+    clock.tick(1999);
     expect(store.state.visible).toBe(true);
-    // it then calls hide which is debounced by 2 seconds
-    setTimeout(() => {
-      expect(store.state.visible).toBe(false);
-      done();
-    }, 2500);
+    clock.tick(1);
+    expect(store.state.visible).toBe(false);
   });
-  it ('Hide Alert', done => {
+
+  it ('Hide Alert', () => {
     store.replaceState({ ...state, visible: true });
-    expect(store.state.visible).toBe(true);
     store.dispatch(Actions.HideAlert);
-    setTimeout(() => {
-      expect(store.state.visible).toBe(false);
-      done();
-    }, 2500);
+    expect(store.state.visible).toBe(true);
+    clock.tick(2000);
+    expect(store.state.visible).toBe(false);
   });
 });
