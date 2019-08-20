7821bab3403614541075d3bc1d6adbe58c43d515
Chris-Robin Ennen <chris@ncteam.de>
2019-08-20 18:32:56 +0200

    button for new script

---
+++ src/components/editor/buttons/ScriptNewButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn :icon="icon" @click="newScript">
+    <v-icon v-if="icon">mdi-new-box</v-icon>
+    <slot v-else>New Scripts</slot>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Mutation } from 'vuex-class';
+import { Mutations } from '@/store/scripts';
+
+@Component
+export default class ScriptNewButton extends Vue {
+  @Prop({ required: false, default: false }) icon!: boolean;
+
+  @Mutation(Mutations.SetSelectedScript)
+  setSelectedScript!: (id: number | null) => void;
+
+  newScript() {
+    this.setSelectedScript(null);
+  }
+}
+</script>
